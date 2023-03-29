670d4481832a77c5b0614f8646bbfbc15f7ba096
Darius <dariuspasca@users.noreply.github.com>
2023-03-29 16:49:46 +0200

    update: added logout button

--- apps/nextjs/src/components/NavigationHeader.tsx
+++ apps/nextjs/src/components/NavigationHeader.tsx
@@ -1,16 +1,25 @@
 import * as React from "react";
 import Link from "next/link";
+import { signOut } from "next-auth/react";
 
 const NavigationHeader: React.FC = () => {
   return (
     <header className="container relative mx-auto p-4">
-      <Link
-        href="/create-post"
-        className="text-3xl font-bold tracking-widest text-gray-950"
-      >
-        ab
-      </Link>
-      <div className="absolute left-4 top-3/4 w-6 border-b-2 border-gray-600"></div>
+      <div className="absolute left-4 top-3/4 w-6 border-b-2 border-gray-600" />
+      <div className="flex justify-between">
+        <Link
+          href="/create-post"
+          className="text-3xl font-bold tracking-widest text-gray-950"
+        >
+          ab
+        </Link>
+        <button
+          className="font-semibold text-gray-900 hover:underline"
+          onClick={() => void signOut()}
+        >
+          Sign out
+        </button>
+      </div>
     </header>
   );
 };
