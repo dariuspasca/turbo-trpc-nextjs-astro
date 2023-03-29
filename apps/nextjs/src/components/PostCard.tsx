d9654c4982b0bb8dcb7ea2f64b90076e9bb5dd47
Darius <dariuspasca@users.noreply.github.com>
2023-03-29 16:44:32 +0200

    update: PostCard styling

--- apps/nextjs/src/components/PostCard.tsx
+++ apps/nextjs/src/components/PostCard.tsx
@@ -11,14 +11,14 @@ const PostCard: React.FC<PostCardProps> = ({ post, onPostDelete }) => {
   const { title, content } = post;
 
   return (
-    <div className="flex flex-row rounded-lg bg-white/10 p-4 transition-all hover:scale-[101%]">
+    <div className="flex flex-row rounded-lg p-4 shadow-xl transition-all hover:scale-[101%]">
       <div className="flex-grow">
-        <h2 className="text-2xl font-bold text-pink-400">{title}</h2>
+        <h2 className="text-2xl font-bold ">{title}</h2>
         <p className="mt-2 text-sm">{content}</p>
       </div>
       <div>
         <span
-          className="cursor-pointer text-sm font-bold uppercase text-pink-400"
+          className="cursor-pointer text-sm font-bold uppercase text-gray-900"
           onClick={onPostDelete}
         >
           Delete
