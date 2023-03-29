a3004a08e93eea911004f1a94b33d5614ac36aef
Darius <dariuspasca@users.noreply.github.com>
2023-03-29 16:05:55 +0200

    add: creat post feature

---
+++ apps/nextjs/src/features/CreatePostForm.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import Link from "next/link";
+
+import { api } from "~/utils/api";
+
+const CreatePostForm: React.FC = () => {
+  const utils = api.useContext();
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const { mutate, error } = api.post.create.useMutation({
+    async onSuccess() {
+      setTitle("");
+      setContent("");
+      await utils.post.all.invalidate();
+    },
+  });
+
+  return (
+    <div className="flex w-full max-w-2xl flex-col p-4">
+      <input
+        className="mb-2 rounded bg-white/10 p-2 text-white"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Title"
+      />
+      {error?.data?.zodError?.fieldErrors.title && (
+        <span className="mb-2 text-red-500">
+          {error.data.zodError.fieldErrors.title}
+        </span>
+      )}
+      <Link href="/yoo">Hello</Link>
+      <input
+        className="mb-2 rounded bg-white/10 p-2 text-white"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        placeholder="Content"
+      />
+      {error?.data?.zodError?.fieldErrors.content && (
+        <span className="mb-2 text-red-500">
+          {error.data.zodError.fieldErrors.content}
+        </span>
+      )}
+      <button
+        className="rounded bg-pink-400 p-2 font-bold"
+        onClick={() => {
+          mutate({
+            title,
+            content,
+          });
+        }}
+      >
+        Create
+      </button>
+    </div>
+  );
+};
+
+export default CreatePostForm;
