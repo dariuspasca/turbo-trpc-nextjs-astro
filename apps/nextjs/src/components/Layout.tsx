1ecbc850cdae3998def209da16a052727c02c322
Darius <dariuspasca@users.noreply.github.com>
2023-03-29 16:28:27 +0200

    update: add NavigationHeader to layout

--- apps/nextjs/src/components/Layout.tsx
+++ apps/nextjs/src/components/Layout.tsx
@@ -1,14 +1,17 @@
 import * as React from "react";
 
+import NavigationHeader from "./NavigationHeader";
+
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <main className="flex h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      {children}
-    </main>
+    <>
+      <NavigationHeader />
+      <main className="flex h-screen flex-col items-center">{children}</main>
+    </>
   );
 };
 
