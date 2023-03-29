d7176e0b40330c0801117e89a5f711c19d6121c8
Darius <dariuspasca@users.noreply.github.com>
2023-03-29 17:03:21 +0200

    fix: header link href dashboard

--- apps/nextjs/src/components/NavigationHeader.tsx
+++ apps/nextjs/src/components/NavigationHeader.tsx
@@ -8,7 +8,7 @@ const NavigationHeader: React.FC = () => {
       <div className="absolute left-4 top-3/4 w-6 border-b-2 border-gray-600" />
       <div className="flex justify-between">
         <Link
-          href="/create-post"
+          href="/"
           className="text-3xl font-bold tracking-widest text-gray-950"
         >
           ab
