71c3733f36d25f5903e817a5d4148b39d32d52c3
Darius <dariuspasca@users.noreply.github.com>
2023-03-29 16:27:55 +0200

    update: add body styling

---
+++ apps/nextjs/src/_document.tsx
@@ -0,0 +1,13 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body className="bg-gray-100 text-gray-900">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
