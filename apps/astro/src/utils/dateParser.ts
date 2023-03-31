6358ffc2a19775faf9a6a4d4a623fae6f2ee4df3
Darius <dariuspasca@users.noreply.github.com>
2023-03-31 16:10:13 +0200

    update: astro show blog posts

---
+++ apps/astro/src/utils/dateParser.ts
@@ -0,0 +1,16 @@
+const MONTHS = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+export const getMonthName = (date: Date) => MONTHS[new Date(date).getMonth()];
